316a88774dd3b2a934d853c48b77dbcfea9f222c
Diego Rodrigues <di.rodrigues003@gmail.com>
2021-04-12 20:02:35 -0400

    Add quiz section

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,5 +1,6 @@
 import Card from '../card/Card'
 
+import quizArt from '../../assets/quiz.svg'
 import './Homepage.css'
 
 const Homepage = () => {
@@ -17,6 +18,13 @@ const Homepage = () => {
                     <Card />
                     <Card />
                     <Card />
+                </div>
+            </div>
+            <div className="quiz-display">
+                <div className="quiz-display__title">Find The Breed That Fits Your Lifestyle</div>
+                <div className="quiz-display__art-btn-wrapper">
+                    <img src={quizArt} className="quiz-display__art"/>
+                    <button className="quiz-display__btn">Take The Quiz</button>
                 </div>
             </div>
         </>
