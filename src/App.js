5d8cefb44a071e2940407d6d92365362f458aa20
Diego Rodrigues <di.rodrigues003@gmail.com>
2021-04-12 20:23:44 -0400

    Add footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Navbar from './components/navbar/Navbar'
 import Homepage from './components/homepage/Homepage'
+import Footer from './components/footer/Footer'
 
 import './Main.css';
 
@@ -16,6 +17,8 @@ function App() {
           {/* Search page */}
         </Switch>
       </Router>
+
+      <Footer />
     </div>
   );
 }
