06bfa993c3b69d85e39011b38095b6dea034f52e
Diego Rodrigues <di.rodrigues003@gmail.com>
2021-04-20 23:58:26 -0400

    Add homepage API call

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState,createContext } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Navbar from './components/navbar/Navbar'
@@ -6,20 +7,46 @@ import Footer from './components/footer/Footer'
 
 import './Main.css';
 
+// React context for API Access Token
+export const AuthContext = createContext();
+
 function App() {
+  const [accessToken, setAccessToken] = useState(null);
+
+  useEffect(() => {
+    const fetchAccessToken = async () => {
+      const params = new URLSearchParams();
+      params.append("grant_type", "client_credentials");
+      params.append("client_id", process.env.REACT_APP_API_KEY);
+      params.append("clent_secret", process.env.REACT_APP_API_SECRETE);
+      fetch(
+        "https://api.petfinder.com/v2/oauth2/token",
+        {
+          method: "POST",
+          body: params
+        }
+      )
+      .then(res => res.json())
+      .then(res => setAccessToken(res.access_token));
+    };
+    fetchAccessToken();
+  }, []);
+
   return (
-    <div className="app">
-      <Navbar />
+    <AuthContext.Provider value={accessToken}>
+      <div className="app">
+        <Navbar />
 
-      <Router>
-        <Switch>
-          <Route exact={true} path="/" component={Homepage}/>
-          {/* Search page */}
-        </Switch>
-      </Router>
+        <Router>
+          <Switch>
+            <Route exact={true} path="/" component={Homepage}/>
+            {/* Search page */}
+          </Switch>
+        </Router>
 
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </AuthContext.Provider>
   );
 }
 
--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,9 +1,35 @@
+import { useState, useEffect, useContext } from 'react'
+import { AuthContext } from '../../App'
 import Card from '../card/Card'
 
 import quizArt from '../../assets/quiz.svg'
 import './Homepage.css'
 
 const Homepage = () => {
+    const [petList, setPetList] = useState(null);
+    const accessToken = useContext(AuthContext);
+
+    useEffect(() => {
+        if (accessToken === null)
+            return;
+        const fetchRandPets = async () => {
+            const randPets = await fetch(
+                "https://api.petfinder.com/v2/animals",
+                {
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`,
+                    }, 
+                }
+            );
+            const pets = await randPets.json();
+            console.log(pets);
+            console.log(pets.animals[0].name);
+            setPetList(pets.animals);
+            console.log(petList[0].name);
+        };
+        fetchRandPets();
+    }, [accessToken]);
+
     return (
         <>
             <div className="banner">
@@ -13,11 +39,11 @@ const Homepage = () => {
             <div className="pet-display">
                 <div className="pet-display__title">Pets Looking For A New Home</div>
                 <div className="pet-display__cards">
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
+                    <Card name={petList[0].name} image={petList[0].primary_photo_cropped.full}/>
+                    <Card name={petList[1].name} image={petList[1].primary_photo_cropped.full}/>
+                    <Card name={petList[2].name} image={petList[2].primary_photo_cropped.full}/>
+                    <Card name={petList[3].name} image={petList[3].primary_photo_cropped.full}/>
+                    <Card name={petList[4].name} image={petList[4].primary_photo_cropped.full}/>
                 </div>
             </div>
             <div className="quiz-display">
