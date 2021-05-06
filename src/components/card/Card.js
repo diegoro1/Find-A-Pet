8ad1b7b261f3a6cb3096f36115ef348860eedb0d
Diego Rodrigues <di.rodrigues003@gmail.com>
2021-05-06 17:02:44 -0400

    Fix fetching to wait

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,7 +1,54 @@
+import React, { useState, useEffect } from 'react'
 import defaultImage from '../../assets/dog4.jpg'
+
 import './Card.css'
 
-const Card = ({ image, name }) => {
+const Card = ({ pet }) => {
+    const [image, setImage] = useState(undefined);
+    const [name, setName] = useState(undefined);
+
+    useEffect(() => {
+        if (pet === undefined)
+            return;
+        validateName(pet);
+        validatePicture(pet);
+    }, [])
+
+
+    const validateName = (pet) => {
+        if (pet === undefined || pet.name === undefined)
+            return;
+
+            console.log("pet name:" + pet.name);
+        
+            setName(pet.name);
+    }
+
+    const validatePicture = (pet) => {
+        if (pet === undefined)
+            return;
+        
+        // tries to get a primary photo, sets a regular photo otherwise 
+        // (if there is another photo)
+        if (pet.primary_photo_cropped === null) {
+            if (pet.photos.length !== 0) {
+                // sets the first picture from object
+                setImage(pet.photos[Object.keys(pet.photos)[0]]);
+            }
+
+            return; 
+        }
+
+        // tries to return the full cropped primary photo, else sets the first
+        // photo in object
+        if (pet.primary_photo_cropped.full !== undefined) {
+            setImage(pet.primary_photo_cropped.full);
+        }
+        else {
+            setImage(pet.primary_photo_cropped[Object.keys(pet.primary_photo_cropped)[0]]);
+        }
+    }
+
     return (
         <div className="card">
             <div className="card__image" style={
--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -10,8 +10,10 @@ const Homepage = () => {
     const accessToken = useContext(AuthContext);
 
     useEffect(() => {
-        if (accessToken === null)
+        if (accessToken === null){
+            console.log("EFFECT!");
             return;
+        }
         const fetchRandPets = async () => {
             const randPets = await fetch(
                 "https://api.petfinder.com/v2/animals",
@@ -22,10 +24,7 @@ const Homepage = () => {
                 }
             );
             const pets = await randPets.json();
-            console.log(pets);
-            console.log(pets.animals[0].name);
             setPetList(pets.animals);
-            console.log(petList[0].name);
         };
         fetchRandPets();
     }, [accessToken]);
@@ -39,11 +38,12 @@ const Homepage = () => {
             <div className="pet-display">
                 <div className="pet-display__title">Pets Looking For A New Home</div>
                 <div className="pet-display__cards">
-                    <Card name={petList[0].name} image={petList[0].primary_photo_cropped.full}/>
-                    <Card name={petList[1].name} image={petList[1].primary_photo_cropped.full}/>
-                    <Card name={petList[2].name} image={petList[2].primary_photo_cropped.full}/>
-                    <Card name={petList[3].name} image={petList[3].primary_photo_cropped.full}/>
-                    <Card name={petList[4].name} image={petList[4].primary_photo_cropped.full}/>
+                    {console.log(petList)}
+                    {petList ?  <Card pet={petList[0]} /> : console.log("Nothing!")}
+                    {petList ?  <Card pet={petList[1]} /> : console.log("Nothing!")}
+                    {petList ?  <Card pet={petList[2]} /> : console.log("Nothing!")}
+                    {petList ?  <Card pet={petList[3]} /> : console.log("Nothing!")}
+                    {petList ?  <Card pet={petList[4]} /> : console.log("Nothing!")}
                 </div>
             </div>
             <div className="quiz-display">
